47a7e5d269599a0bfa490332a464717f4c4abe26
King James Castro <castro.kingjames@gmail.com>
2025-05-02 13:21:04 +0800

    Update index.ts

--- server/src/index.ts
+++ server/src/index.ts
@@ -3,7 +3,7 @@ import { Server } from "colyseus";
 import { createServer } from "http";
 import FreeForAll from './rooms/FreeForAll';
 
-const port = 2560
+const port = process.env.PORT || 500
 
 const app = express();
 app.use(express.json());
